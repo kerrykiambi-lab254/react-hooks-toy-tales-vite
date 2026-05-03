7553c739d441a7307252113f732fc84d443f7ad1
kerrykiambi-lab254 <kerry.kiambi@student.moringaschool.com>
2026-05-03 20:39:31 +0300

    twerk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,72 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then(setToys);
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddToy(toy) {
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...toy, likes: toy.likes ?? 0 }),
+    })
+      .then((response) => response.json())
+      .then((savedToy) => {
+        setToys((toys) => [...toys, { ...savedToy, likes: savedToy.likes ?? 0 }]);
+      });
+  }
+
+  function handleDeleteToy(id) {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok) {
+        setToys((toys) => toys.filter((toy) => toy.id !== id));
+      }
+    });
+  }
+
+  function handleLikeToy(toy) {
+    const updatedLikes = toy.likes + 1;
+
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes: updatedLikes }),
+    })
+      .then((response) => response.json())
+      .then((updatedToy) => {
+        setToys((toys) =>
+          toys.map((currentToy) =>
+            currentToy.id === updatedToy.id
+              ? { ...currentToy, likes: updatedToy.likes ?? updatedLikes }
+              : currentToy
+          )
+        );
+      });
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onToySubmit={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} onDeleteToy={handleDeleteToy} onLikeToy={handleLikeToy} />
     </>
   );
 }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onToySubmit }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!name || !image) return;
+
+    onToySubmit({ name, image });
+    setName("");
+    setImage("");
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
         />
         <br />
         <input
@@ -17,14 +32,13 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={image}
+          onChange={(event) => setImage(event.target.value)}
         />
         <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create New Toy"
-          className="submit"
-        />
+        <button type="submit" className="submit">
+          Create New Toy
+        </button>
       </form>
     </div>
   );
